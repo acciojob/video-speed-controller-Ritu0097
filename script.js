e584b08ab966e37f60146f64507bbcaa1271b073
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-03 15:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const bac10 = document.getElementById("back10");
+bac10.addEventListener("click",()=>{
+	video.currentTime -= 10s;
+});
+const for25 = document.getElementById("forward25");
+for25.addEventListener("click", ()=>{
+	video.currentTime += 25s;
+});
