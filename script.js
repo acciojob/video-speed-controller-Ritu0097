a2eae86f4d6b6a445ce8608ea2700c5c80ac5edc
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-04 10:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,21 +7,3 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-
-    const speed = document.querySelector('.speed');
-const bar = speed.querySelector('.speed-bar');
-const video = document.querySelector('.flex');
-
-function handleMove(e) {
-  const y = e.pageY - this.offsetTop;
-  const percent = y / this.offsetHeight;
-  const min = 0.5;
-  const max = 4;
-  const height = Math.round(percent * 100) + '%';
-  const playbackRate = percent * (max - min) + min;
-  bar.style.height = height;
-  bar.textContent = playbackRate.toFixed(1) + '×';
-  video.playbackRate = playbackRate;
-}
-
-speed.addEventListener('mousemove', handleMove);
